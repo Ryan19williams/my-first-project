aea58a9630e7989ae6471dc11ca9e32d29ac8198
Ryan williams <35343573+Ryan19williams@users.noreply.github.com>
2020-02-05 14:04:43 +0000

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,24 @@
+// let coffeeOrder = [
+//     "   Liam - cappuccino",
+//     "Leona - cortado",
+//     "Charlie - Whatever's new"
+// ];
+// console.log(coffeeOrder)
+
+// let fravouriteMusic = [
+//     "Andrew - partyhard",
+//     "hero - chassing",
+//     "--- - save me",
+// ]
+
+// console.log (fravouriteMusic)
+
+//@ts-check
+// let fravouriteWebSite = [
+//     " YouTube",
+//      " 9anime",
+//      "crunchyroll",
+//     ] 
+// console.log (fravouriteWebSite)
+
+
